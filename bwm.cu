c43f18d9904639c6f270cfcb8d6164aa693e2edb
Zeege-0 <992793636@qq.com>
2022-11-19 18:54:59 +0800

    initial commit

---
+++ bwm.cu
@@ -0,0 +1,72 @@
+#include <cuda_runtime.h>
+#include <iostream>
+#include "svd_3x3.cu"
+
+#define IDX(i, j, ld) (((i) * (ld)) + j)
+
+
+/**
+ * A = U * S * VT, the VT is transposed, all matrix has **continous** address space
+ * 
+ * @param A dense input
+ * @param lda leading dimension of A
+ * @param U dense matrix U
+ * @param S dense vector V
+ * @param VT dense matrix VT
+*/
+__device__ __forceinline__
+void svd_continous(float *A, float *U, float *S, float *VT){
+    svd(A[0], A[1], A[2], A[3], A[4], A[5], A[6], A[7], A[8],
+        U[0], U[1], U[2], U[3], U[4], U[5], U[6], U[7], U[8],
+        S[0], S[1], S[2],
+        VT[0], VT[3], VT[6], VT[1], VT[4], VT[7], VT[2], VT[5], VT[8]);
+}
+
+/**
+ * @param A CWH stored, row major
+ * @param rows num of rows
+ * @param cols num of cols
+ * @param lda leading dimension of A
+*/
+__global__ void embed_gpu(float *A, size_t rows, size_t cols, size_t lda, float *U, float *S, float *VT){
+
+    const int TILE_DIM = 3;
+    int i_stride = TILE_DIM * blockDim.x * gridDim.x;
+    int j_stride = TILE_DIM * blockDim.y * gridDim.y;
+
+    __shared__ float sA[rows * cols];
+
+    for(int cid = 0; cid < 3; ++ cid){
+        float *channel = A + cid * rows * cols;
+        for(int ti = threadIdx.x + blockIdx.x * blockDim.x; ti < rows / TILE_DIM; ti += i_stride){
+            for(int tj = threadIdx.y + blockIdx.y * blockDim.y; tj < cols / TILE_DIM; tj += j_stride){
+
+                // start address of the tile
+                int i_tile = ti * TILE_DIM;
+                int j_tile = tj * TILE_DIM;
+                
+                // copy to shared memory, remap address so each tile has **continous** address space
+                sA[IDX(i_tile, j_tile, lda) + 0] = channel[IDX(i_tile + 0, j_tile + 0, lda)];
+                sA[IDX(i_tile, j_tile, lda) + 1] = channel[IDX(i_tile + 0, j_tile + 1, lda)];
+                sA[IDX(i_tile, j_tile, lda) + 2] = channel[IDX(i_tile + 0, j_tile + 2, lda)];
+                sA[IDX(i_tile, j_tile, lda) + 3] = channel[IDX(i_tile + 1, j_tile + 0, lda)];
+                sA[IDX(i_tile, j_tile, lda) + 4] = channel[IDX(i_tile + 1, j_tile + 1, lda)];
+                sA[IDX(i_tile, j_tile, lda) + 5] = channel[IDX(i_tile + 1, j_tile + 2, lda)];
+                sA[IDX(i_tile, j_tile, lda) + 6] = channel[IDX(i_tile + 2, j_tile + 0, lda)];
+                sA[IDX(i_tile, j_tile, lda) + 7] = channel[IDX(i_tile + 2, j_tile + 1, lda)];
+                sA[IDX(i_tile, j_tile, lda) + 8] = channel[IDX(i_tile + 2, j_tile + 2, lda)];
+
+                // addr of the embedding 3x3 slice
+                float *tile = &sA[IDX(i_tile, j_tile, lda)]; 
+                float *tileU = &U[IDX(i_tile, j_tile, lda)];
+                float *tileVT = &VT[IDX(i_tile, j_tile, lda)];
+                float *tileS = &S[IDX(ti, j_tile, cols)]; // note that S is vector
+                svd_continous(tile, tileU, tileS, tileVT);
+                
+            }
+        }
+    }
+    
+}
+
+
---
+++ myutils.cpp
@@ -0,0 +1,85 @@
+
+#include <iostream>
+#include <sys/stat.h>
+#include <fstream>
+
+#ifndef IDX
+#define IDX(i, j, ld) (((i) * (ld)) + j)
+#endif
+
+void print_matrix(float *A, int rows, int cols){
+    for(int i = 0; i < rows; ++i){
+        for(int j = 0; j < cols; ++j){
+            std::cout << A[IDX(i, j, cols)] << ", ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void print_vector(float *A, int n){
+    for(int i = 0; i < n; ++i){
+        std::cout << A[i] << ", ";
+    }
+    std::cout << std::endl;
+}
+
+
+char *readbin(const std::string &filePath, size_t *fileSize, void *buffer, size_t bufferSize) {
+    std::cout << "Begin read " << filePath << std::endl;
+    struct stat sBuf;
+    int fileStatus = stat(filePath.data(), &sBuf);
+    if (fileStatus == -1) {
+        std::cout << "failed to get file" << std::endl;
+        return nullptr;
+    }
+    if (S_ISREG(sBuf.st_mode) == 0) {
+        std::cout << filePath.c_str() << " is not a file, please enter a file" << std::endl;
+        return nullptr;
+    }
+
+    std::ifstream file;
+    file.open(filePath, std::ios::binary);
+    if (!file.is_open()) {
+        std::cout << "Open file failed. path " << filePath.c_str() << std::endl;
+        return nullptr;
+    }
+
+    std::filebuf *buf = file.rdbuf();
+    size_t size = buf->pubseekoff(0, std::ios::end, std::ios::in);
+    if (size == 0) {
+        std::cout << "file size is 0" << std::endl;
+        file.close();
+        return nullptr;
+    }
+    std::cout << "File size = " << size << ", buffer size = " << bufferSize << std::endl;
+    if (size > bufferSize) {
+        std::cout << "file size = " << size << " is larger than buffer size = " << bufferSize << std::endl;
+        file.close();
+        return nullptr;
+    }
+    buf->pubseekpos(0, std::ios::in);
+    buf->sgetn(static_cast<char *>(buffer), size);
+    *fileSize = size;
+    file.close();
+    std::cout << "Finish read file" << std::endl;
+    return static_cast<char *>(buffer);
+}
+
+bool writebin(const std::string &filePath, const void *buffer, size_t size) {
+    if (buffer == nullptr) {
+        // ERROR_LOG("Write file failed. buffer is nullptr");
+        return false;
+    }
+
+    FILE *outputFile = fopen(filePath.c_str(), "wb");
+    if (outputFile == nullptr) {
+        // ERROR_LOG("Open file failed. path = %s", filePath.c_str());
+        return false;
+    }
+
+    fwrite(buffer, size, sizeof(char), outputFile);
+    fclose(outputFile);
+
+    return true;
+}
+
