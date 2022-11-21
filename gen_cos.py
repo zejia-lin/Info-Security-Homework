52c8e8c2c540720771bea5b65c3750bc67e53c59
Zeege-0 <992793636@qq.com>
2022-11-21 14:11:06 +0800

    using double accumulator

--- gen_cos.py
+++ gen_cos.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-N = 3
+N = 4
 
 
 for i in range(N):
     for j in range(N):
-        print(np.cos((2 * i + 1) * j * np.pi / (2 * N)), end=', ')
+        print("{0:.52f}".format(np.cos((2 * i + 1) * j * np.pi / (2 * N))), end=', ')
         if j == N - 1:
             print()
