e8d148532ef8302d0195fb6e9e9cfa32dd270718
Zeege-0 <992793636@qq.com>
2022-12-12 15:32:28 +0800

    revert video

--- demo/test_video.py
+++ demo/test_video.py
@@ -5,23 +5,10 @@ import os
 import sys
 import tqdm
 import time
-import heapq
-import multiprocessing
 from skimage.metrics import mean_squared_error
 import pandas as pd
 from test_tool import *
 from make_report import make_report
-
-
-def embed_async(wid):
-    picname = os.path.join(tmpdir, f'lzj_tmp_{wid}.jpg')
-    outname = os.path.join(tmpdir, f'lzj_emb_{wid}.jpg')
-    cmd = f"embed {picname} {wmname} {outname}\n".encode('utf-8')
-    servers[wid].stdin.write(cmd)
-    servers[wid].stdin.flush()
-    echo = servers[wid].stdout.readline()
-    pbar.update(1)
-
 
 basedir = os.path.dirname(__file__)
 picdir = os.path.join(basedir, 'pic')
@@ -34,26 +21,23 @@ watermark_name = 'logo.jpg'
 wmname = os.path.join(wmdir, watermark_name)
 wmmat = cv2.imread(wmname)
 wmrows, wmcols = wmmat.shape[:2]
-WORKERS = 2
-
 
 ############################################################
 # Start CLI
 ############################################################
 server_st = time.time()
 servers = []
-for _ in range(WORKERS):
-    servers.append(subprocess.Popen(f'../build/WmCLI', shell=True,
+server = subprocess.Popen(f'../build/WmCLI', shell=True,
                     stdin=subprocess.PIPE, 
                     stdout=subprocess.PIPE, 
-                    stderr=subprocess.STDOUT))
-    for i in range(2):
-        print(servers[_].stdout.readline().decode('utf-8'), end='')
+                    stderr=subprocess.STDOUT)
+for _ in range(2):
+    print(server.stdout.readline().decode('utf-8'), end='')
 print(f'Startup time: {(time.time() - server_st) * 1000} ms')
-print(f"Created {WORKERS} workers")
+
 
 ############################################################
-# Init video capture
+# Embed
 ############################################################
 cap = cv2.VideoCapture(video_path)
 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -63,38 +47,35 @@ fourcc = cap.get(cv2.CAP_PROP_FOURCC)
 fps =  cap.get(cv2.CAP_PROP_FPS)
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 video_writer = cv2.VideoWriter(output_path, int(fourcc), fps, frame_size)
+
 count = 0
 pbar = tqdm.tqdm(total=length)
 e2est = time.time()
 emtimeacc = 0
-pool = multiprocessing.Pool(WORKERS)
-futures = []
-
-
 while cap.isOpened():
-    for wid in range(WORKERS):
-        ret, frame = cap.read()
-        if not ret:
-            break
-        count += 1
-        picname = os.path.join(tmpdir, f'lzj_tmp_{wid}.jpg')
-        outname = os.path.join(tmpdir, f'lzj_emb_{wid}.jpg')
-        cv2.imwrite(picname, frame)
-        future = pool.apply_async(embed_async, (wid,))
-        futures.append(future)
-    for future in futures:
-        future.wait()
-    futures = []
-    for wid in range(WORKERS):
-        img = cv2.imread(os.path.join(tmpdir, f'lzj_emb_{wid}.jpg'))
-        video_writer.write(img)
+    ret, frame = cap.read()
+    if not ret:
+        break
+    count += 1
+    pbar.update(1)
+    picname = os.path.join(tmpdir, 'lzj_tmp.jpg')
+    outname = os.path.join(tmpdir, 'lzj_emb.jpg')
+    cv2.imwrite(picname, frame)
+    tmpst = time.time()
+    cmd = f"embed {picname} {wmname} {outname}\n".encode('utf-8')
+    server.stdin.write(cmd)
+    server.stdin.flush()
+    echo = server.stdout.readline()
+    tmped = time.time()
+    emtimeacc += tmped - tmpst
+    wmed_frame = cv2.imread(outname)
+    video_writer.write(wmed_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 video_writer.release()
 pbar.close()
 e2eed = time.time()
-os.system('rm /tmp/shm/lzj_*')
 
 print("End to end", e2eed - e2est)
 print("Embed", emtimeacc)
